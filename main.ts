29af11aa49e5919dbf6b740da0fd18c3babb3a0b
Patrick Coxall <mr.coxall@mths.ca>
2025-10-08 12:09:24 -0400

    init

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mr Coxall
+ * Created on: Sep 2025
+ * This program used NeoPixels
 */
 
-basic.showString('Hello, World!')
+basic.showIcon(IconNames.Happy)
