e6169da727635d536ac82baa4631fe9a0749eebe
Patrick Coxall <mr.coxall@mths.ca>
2025-10-08 12:46:52 -0400

    update

--- main.ts
+++ main.ts
@@ -6,8 +6,19 @@
 */
 
 // variables
-
+let neopixelStrip: neopixel.Strip = null
 
 // cleanup
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+
+input.onButtonPressed(Button.A, function() {
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+    neopixelStrip.show()
+})
